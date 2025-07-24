ba9375100b93b078d5d9f89b140e400ce0ef1c07
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-24 19:56:45 -0300

    feat: purchase flow controller

---
+++ src/main/kotlin/com/dolina_orchestrator/controllers/PurchaseItemController.kt
@@ -0,0 +1,21 @@
+package com.dolina_orchestrator.controllers
+
+import com.dolina_orchestrator.dtos.StartFlowPurchaseDto
+import com.dolina_orchestrator.services.PurchaseService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/purchase")
+class PurchaseItemController(private val purchaseService: PurchaseService) {
+
+    @PostMapping
+    fun purchaseItem(@RequestBody payload: StartFlowPurchaseDto): ResponseEntity<String> {
+        purchaseService.purchaseItem(payload)
+        return ResponseEntity.status(HttpStatus.OK).body("Ebaa")
+    }
+}
