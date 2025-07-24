655917f246f8e79ea2338683c6029d40cd429b67
Bruna Suzuki <brunasuzuki04@gmail.com>
2025-07-24 19:57:02 -0300

    feat: purchase flow service

---
+++ src/main/kotlin/com/dolina_orchestrator/services/PurchaseService.kt
@@ -0,0 +1,23 @@
+package com.dolina_orchestrator.services
+
+import com.dolina_orchestrator.dtos.StartFlowPurchaseDto
+import com.dolina_orchestrator.dtos.requests.RequestValidateItemDto
+import com.dolina_orchestrator.dtos.requests.RequestValidateUserDto
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.stereotype.Service
+
+@Service
+class PurchaseService(
+    private val itemValidatorClient: ItemValidatorClient,
+    private val userValidatorClient: UserValidatorClient) {
+
+    fun purchaseItem(payload: StartFlowPurchaseDto) {
+        val itemRequest = RequestValidateItemDto(payload.globalItemId)
+        val itemValidator = itemValidatorClient.itemValidator(itemRequest)
+        println(itemValidator)
+
+        val userRequest = RequestValidateUserDto(payload.globalUserId, itemValidator!!.itemPrice)
+        val userValidator = userValidatorClient.userValidator(userRequest)
+        println(userValidator)
+    }
+}
